ce7890ca51251b706c179f4b67e199dde3f18cc5
steven.jiang@kii.com <steven.jiang@kii.com>
2017-05-26 18:03:34 +0800

    add file download/upload demo

--- bas-web-portal/src/main/java/com/kii/bas/portal/web/controller/SchemaResourceController.java
+++ bas-web-portal/src/main/java/com/kii/bas/portal/web/controller/SchemaResourceController.java
@@ -1,18 +1,28 @@
 package com.kii.bas.portal.web.controller;
 
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import javax.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.kii.bas.portal.web.entity.ResourceEntry;
 
 @Controller
 @RequestMapping(path = {"/schema/resources"}, consumes = {MediaType.ALL_VALUE}, produces = {
@@ -20,33 +30,55 @@ import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBo
 public class SchemaResourceController {
 	
 	
-	public void addResourceToPoint() {
-		
-	}
+	@Autowired
+	private ResourceLoader loader;
 	
-	
-	@RequestMapping(path = {"/{bindName}"}, method = RequestMethod.GET, consumes = {MediaType.ALL_VALUE}, produces = {
-			MediaType.APPLICATION_JSON_UTF8_VALUE})
-	public StreamingResponseBody getResourceFromPoint(@PathVariable("bindName") String name, WebRequest request) {
+	@PostMapping("/{bindName}")
+	public String handleFormUpload(@RequestParam("description") String description,
+								   @RequestParam("file") MultipartFile file) {
 		
-		
-		long lastModified = // 1. application-specific calculation
-		
-		if (request.checkNotModified(lastModified)) {
-			// 2. shortcut exit - no further processing necessary
-			return null;
+		if (!file.isEmpty()) {
+			try {
+				byte[] bytes = file.getBytes();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			// store the bytes somewhere
+			return "redirect:uploadSuccess";
 		}
 		
+		return "redirect:uploadFailure";
+	}
+	
+	@ResponseBody
+	@RequestMapping(path = {"/{bindName}"}, method = RequestMethod.GET, consumes = {MediaType.ALL_VALUE}, produces = {
+			MediaType.APPLICATION_JSON_UTF8_VALUE})
+	public ResponseEntity getResourceFromPoint(@PathVariable("bindName") String name, WebRequest request, HttpServletResponse response) {
 		
-		return outptream -> {
+		
+		ResourceEntry entry = new ResourceEntry();
+		entry.setMediaType(MediaType.IMAGE_PNG_VALUE);
+//		entry.setPath(new File("/Users/steven/workspace/bas-backend/bas-web-portal/src/test/resources/demo.png"));
+		entry.setSize(27945l);
+		entry.setLastModify(new Date());
+		
+		
+		if (request.checkNotModified(entry.getLastModify().getTime())) {
 			
-			File file = new File("?");
-			try (FileInputStream input = new FileInputStream(file)
-			) {
-				StreamUtils.copy(new FileInputStream(file), outptream);
-			} catch (IOException e) {
-				
-			}
-		};
+			ResponseEntity entity = ResponseEntity.status(HttpStatus.NOT_MODIFIED).build();
+			
+			return entity;
+		}
+		
+		Resource resource = loader.getResource("file:///Users/steven/workspace/bas-backend/bas-web-portal/src/test/resources/demo.png");
+		
+		
+		ResponseEntity<Resource> entity = ResponseEntity
+				.ok()
+				.contentLength(entry.getSize())
+				.contentType(entry.getMediaType())
+				.lastModified(entry.getLastModify().getTime())
+				.body(resource);
+		return entity;
 	}
 }
