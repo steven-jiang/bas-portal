c27f162a10540f6f50d70e8cf28b0d0c39c7b107
steven.jiang@kii.com <steven.jiang@kii.com>
2017-05-31 15:59:41 +0800

    modify mongo init follow quick fail

--- bas-commons/src/main/java/com/kii/bas/commons/store/mongo/MongoClientFactory.java
+++ bas-commons/src/main/java/com/kii/bas/commons/store/mongo/MongoClientFactory.java
@@ -1,11 +1,15 @@
 package com.kii.bas.commons.store.mongo;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 
 import com.mongodb.MongoClient;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoDatabase;
+
 
 public class MongoClientFactory {
 	
@@ -16,21 +20,44 @@ public class MongoClientFactory {
 	@Value("${store.mongoDB.dbName}")
 	private String dbName;
 	
-	@Bean
-	public MongoDatabase getClient() {
+	
+	private MongoClient mongoClient;
+	
+	private com.mongodb.async.client.MongoClient asyncClient;
+	
+	
+	@PostConstruct
+	public void init() {
+		
 		
 		MongoClientURI connectionString = new MongoClientURI(url);
 		
 		
-		MongoClient mongoClient = new MongoClient(connectionString);
+		mongoClient = new MongoClient(connectionString);
+		
+		asyncClient = com.mongodb.async.client.MongoClients.create(url);
+		
+	}
+	
+	
+	@PreDestroy
+	public void close() {
+		
+		mongoClient.close();
+		
+		asyncClient.close();
+	}
+	
+	@Bean
+	public MongoDatabase getClient() {
+		
 		
 		return mongoClient.getDatabase(dbName);
 	}
 	
 	@Bean
 	public com.mongodb.async.client.MongoDatabase getAsyncClient() {
-		com.mongodb.async.client.MongoClient mongoClient = com.mongodb.async.client.MongoClients.create(url);
 		
-		return mongoClient.getDatabase(dbName);
+		return asyncClient.getDatabase(dbName);
 	}
 }
